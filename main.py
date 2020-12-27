3f41761490d1ea6849110a312b84a79eb3d3240c
Eric E <ethridge2004@gmail.com>
2020-12-27 02:24:52 -0500

    reset main

--- main.py
+++ main.py
@@ -1,54 +1,31 @@
 # Outcome: load simple webpage
 import datetime
-
 from flask import Flask, render_template
 from google.cloud import firestore
 import datetime
 import uuid
+import logging
 
 app = Flask(__name__)
 
-# Add new document
-
-db = firestore.Client()
-doc_ref = db.collection(u'times').document(str(x))
-
-
-def store_time():
-    x = datetime.datetime.now()
-    v = uuid.uuid1()
-    return doc_ref.set({u'id': str(v), u'timestamp': x})
-
-
-def fetch_times():
-    query = db.collection(u'times').order_by(u'timestamps').limit(10).stream
-    return query
-
-
-@app.route('/fetchtimes')
-def root():
-    fetchtimes = db.collection(u'times').order_by(u'timestamps').limit(
-        10).stream
-    return render_template('index.html', times=fetchtimes)
-
 
 @app.route('/')
-def root():
-    # For the sake of example, use static information to inflate the template.
-    # This will be replaced with real information in later steps\
+def hello():
+    """Return a friendly greeting"""
+    return 'Hello World!'
 
-    dummy_times = [
-        datetime.datetime(2018, 1, 1, 10, 0, 0),
-        datetime.datetime(2018, 1, 2, 10, 30, 0),
-        datetime.datetime(2018, 1, 3, 11, 0, 0),
-    ]
 
-    return render_template('index.html', times=dummy_times)
+@app.errorhandler(500)
+def server_error(e):
+    logging.exception('An error did some shit')
+    return """
+    An internal and shitty error occured: <pre>{}</pre>
+    See logs for a full and complete stack trace.
+    """.format(e), 500
 
 
 if __name__ == '__main__':
-    # This is used when running locally only. When deploying to Google App
-    # Engine, a webserver process such as Gunicorn will serve the app. This
-    # can be configured by adding an `entrypoint` to app.yaml.
-
-    app.run(host='127.0.0.1', port=8080, debug=True)
+    # We use this shit when it runs locally
+    # In a 'development' fashion
+    # Gunicorn is the standard app server for app engine
+    app.run(host='127.0.0.1', post=8080, debug=True)
